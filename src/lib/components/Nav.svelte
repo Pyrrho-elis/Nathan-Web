c732b99f9d766b71e20366798ccd3f360b79a2a0
Pyrrho-elis <naolm75@gmail.com>
2024-06-11 22:15:18 +0300

    First commit, theme colors not done

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { toggleMode } from 'mode-watcher';
+	import { Sun, Moon } from 'svelte-radix';
+    import Button from './ui/button/button.svelte';
+</script>
+
+<nav class="sticky flex items-center justify-between p-4 text-xl backdrop-blur-sm m-4">
+	<h1 class="text-3xl font-bold"><a href="/">Nathan</a></h1>
+	<ul class="flex gap-8 items-center mx-4 text-xl">
+		<li><a href="/">Home</a></li>
+		<li><a href="/projects">Projects</a></li>
+		<li><a href="/me">About</a></li>
+        <li>
+			<Button on:click={toggleMode} variant="outline" size="icon">
+				<Sun
+					class="scale-100 transition-all dark:-rotate-90 dark:scale-0"
+				/>
+				<Moon
+					class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+				/>
+			</Button>
+		</li>
+	</ul>
+</nav>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+    import Nav from "$lib/components/Nav.svelte"
+    import Autoplay from "embla-carousel-autoplay"
+    import * as Carousel from "$lib/components/ui/carousel/index"
+    import * as Card from "$lib/components/ui/card/index"
+</script>
+
+<div class="h-screen">
+    <Nav />
+    <div class='flex m-4 p-4 lg:justify-between sm:items-center sm:justify-center'>
+        <h1 class="text-4xl text-wrap font-black lg:w-1/3 sm:w-2/3 sm:text-center">Video Editing, 3D Modelling, Drawing... I do it all</h1>
+        <div class="w-1/3">
+            <Carousel.Root plugins={[
+                Autoplay({
+                  delay: 5000,
+                }),
+              ]}>
+                <Carousel.Content>
+                  {#each Array(5) as _, i (i)}
+                    <Carousel.Item>
+                        <Card.Root>
+                            <Card.Content>
+                                <Card.Title>
+                                    <h2>Title</h2>
+                                </Card.Title>
+                                <Card.Description>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                </Card.Description>
+                            </Card.Content>
+                        </Card.Root>
+                    </Carousel.Item>
+                  {/each}
+                </Carousel.Content>
+              </Carousel.Root>
+        </div>
+    </div>
+</div>
